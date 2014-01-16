87cf962a836a325dabfca42a5c311aeac989236e
iivorait <iivo.raitahila@cs.helsinki.fi>
2014-01-16 14:34:45 +0200

    nb-projektin lisays

---
+++ telamiinaharava/src/main/java/fi/helsinki/cs/telamiinaharava/App.java
@@ -0,0 +1,13 @@
+package fi.helsinki.cs.telamiinaharava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
