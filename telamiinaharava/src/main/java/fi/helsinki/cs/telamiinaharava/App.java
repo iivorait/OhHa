0d8f49aa2b031a4c4d700f6f79779c30219b994e
iivorait <iivo.raitahila@cs.helsinki.fi>
2014-01-22 19:33:04 +0200

    ensimmaiset koodit

--- telamiinaharava/src/main/java/fi/helsinki/cs/telamiinaharava/App.java
+++ telamiinaharava/src/main/java/fi/helsinki/cs/telamiinaharava/App.java
@@ -1,13 +1,35 @@
 package fi.helsinki.cs.telamiinaharava;
 
+import java.util.Scanner;
+import sovelluslogiikka.Peli;
+
 /**
- * Hello world!
+ * Testikoodia
  *
  */
 public class App 
 {
     public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    {        
+        Scanner scanner = new Scanner(System.in);
+        
+        Peli peli = new Peli(8);
+        
+        peli.tulosta();
+        
+        while(peli.kaynnissa()) {
+            System.out.println("");
+            System.out.print("Aseta räjähde x: ");
+            int x = Integer.parseInt(scanner.nextLine());
+            System.out.print("Aseta räjähde y: ");
+            int y = Integer.parseInt(scanner.nextLine());
+            System.out.println("");
+
+            peli.rajayta(x, y);
+            peli.tulosta();
+        }
+        
+        System.out.println("\nPeli loppui");
+        
     }
 }
